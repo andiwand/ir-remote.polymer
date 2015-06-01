dea402bbd3368ebbfa431b0b95cf11302bc08b07
andiwand <stefl.andreas@gmail.com>
2015-06-01 16:38:43 +0200

    fix gridster

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ var edit;
 var drag;
 
 $(function() {
-  gridster = $(".gridster > ul").gridster({
+  initGrister();
+
+  selectMenu("remote");
+});
+
+function initGrister() {
+  $("#grid").html("<ul></ul>");
+  gridster = $("#grid > ul").gridster({
     widget_class: "button",
     widget_margins: [5, 5],
     widget_base_dimensions: [50, 50],
@@ -16,13 +23,30 @@ $(function() {
   addButton();
   addButton();
   switchEdit();
+}
 
-  menuSelect(document.getElementById("menu").items[0]);
-});
+function initPage(tag) {
+  if (tag === "remote") initGrister();
+}
+
+function selectMenu(tag) {
+  var menuTagName = $("#menu").attr("attrForSelected");
+  var pagesTagName = $("#pages").attr("attrForSelected");
+  var menuItem = $("#menu").find("[" + menuTagName + "='" + tag + "']").get(0);
+  var page = $("#pages").find("[" + pagesTagName + "='" + tag + "']").get(0);
+  var menuIndex = $("#menu").get(0).indexOf(menuItem);
+  var pageIndex = $("#pages").get(0).indexOf(page);
+
+  $("#menu").get(0).select(menuIndex);
+  $("#pages").get(0).select(pageIndex);
+
+  initPage(tag);
+}
 
 function menuSelect(menu) {
-  var index = menu.getAttribute("index");
-  $("#pages").get(0).select(index);
+  var tagName = $("#menu").attr("attrForSelected");
+  var tag = menu.getAttribute(tagName);
+  selectMenu(tag);
 }
 
 // TODO: do it with onmousedown
@@ -78,7 +102,7 @@ function clickButton(event) {
 
 function toggleEditDialog() {
   console.log($(".edit-dialog"));
-  $(".edit-dialog").get(0).toggle();
+  $("#edit-dialog").get(0).toggle();
 }
 
 function saveEdit() {
